a4a4171ef7c0d9d85fbb8105ab349e4ac5feabaf
Stylll <stephen.aribaba@yahoo.com>
2020-05-17 12:23:00 +0100

    setup basic structure

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from 'react';
+
+import './App.css';
+
+export function App (): ReactElement {
+    return (
+        <div className="App">
+            Content
+        </div>
+    );
+}
